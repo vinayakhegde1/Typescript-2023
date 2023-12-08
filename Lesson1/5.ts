5f1e2b3ff5b2ec496374a8acdbdd424bd6686691
vinayakhegde1 <vinayakhegde619@gmail.com>
2023-12-09 04:07:26 +0530

    Lesson1: 5.ts

---
+++ Lesson1/5.ts
@@ -0,0 +1,24 @@
+// Variabke Tyoe Annotations 
+// We can fix the type any assigned values to a variable
+// We can tell TS what type something is or will be by using 
+// a type annotation 
+// Variables have type annotations (also known as type declarations)
+// added just after their names 
+// type annotation can be define with a (:) and type (e.g, number,string,or any)
+
+// Example
+let mustBeAstring : string;
+mustBeAstring = 'Catdog';
+
+mustBeAstring = 2023;
+//Error: Type 'number' is not assignable to type 'string'
+
+// example 2 
+let phoneNumber;
+
+if(Math.random() > 0.5){
+    phoneNumber = '+61771012062';
+
+}else{
+    phoneNumber = 7167762323;
+}
